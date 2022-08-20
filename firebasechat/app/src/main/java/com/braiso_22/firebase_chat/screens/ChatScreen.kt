d00ec52d96b7ccf00acd628eede2d76deaf1b4f5
braiso_22 <braisfv22@gmail.com>
2022-08-20 22:31:34 +0200

    Created chatScreen

---
+++ firebasechat/app/src/main/java/com/braiso_22/firebase_chat/screens/ChatScreen.kt
@@ -0,0 +1,30 @@
+package com.braiso_22.firebase_chat.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Composable
+@Destination(route = "chat")
+fun ChatScreen() {
+    Scaffold() {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Hello World!", fontSize = 20.sp)
+        }
+    }
+}
+
