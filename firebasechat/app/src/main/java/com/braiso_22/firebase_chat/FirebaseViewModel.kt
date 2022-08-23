78f9a8175afda4889f00d2845ea2da8110a441df
braiso_22 <braisfv22@gmail.com>
2022-08-23 23:38:00 +0200

    FirebaseViewModel for save and retrieve data

---
+++ firebasechat/app/src/main/java/com/braiso_22/firebase_chat/FirebaseViewModel.kt
@@ -0,0 +1,64 @@
+package com.braiso_22.firebase_chat
+
+import android.util.Log
+import com.braiso_22.firebase_chat.model.User
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+val firebaseViewModel = FirebaseViewModel()
+
+class FirebaseViewModel {
+    private val usersCollection = Firebase.firestore.collection("users")
+
+
+    fun getAllUsers(ui: (List<User>) -> Unit) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val querySnapshot = usersCollection.get().await()
+            val arrayUsers = mutableListOf<User>()
+            for (document in querySnapshot.documents) {
+                arrayUsers.add(document.toObject<User>()!!)
+            }
+            ui(arrayUsers)
+
+        } catch (e: Exception) {
+
+        }
+    }
+
+    fun saveUser(user: User) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val querySnapshot = usersCollection.get().await()
+            for (document in querySnapshot.documents) {
+                if (document.toObject<User>()!! == user) {
+                    return@launch
+                }
+            }
+            usersCollection.add(user)
+
+        } catch (e: Exception) {
+
+        }
+    }
+
+    fun suscribeToRealtimeUpdates(setData: (list: MutableList<User>) -> Unit) {
+        val arrayUsers = mutableListOf<User>()
+        usersCollection.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+            firebaseFirestoreException?.let {
+                it.message?.let { it1 -> Log.e(this.toString(), it1) }
+                return@addSnapshotListener
+            }
+            querySnapshot?.let {
+                for (document in querySnapshot.documents) {
+                    arrayUsers.add(document.toObject<User>()!!)
+                }
+                setData(arrayUsers)
+                arrayUsers.clear()
+            }
+        }
+    }
+}
